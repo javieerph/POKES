9752815ca083ba07a9905c6866486cdb283bdb86
Javier <javieer55ph@gmail.com>
2026-08-03 00:37:30 +0100

    formulario oculto

--- inicio.js
+++ inicio.js
@@ -1,3 +1,7 @@
-let clientes = [];
+const btnCrearCliente = document.querySelector('#clientes-crear');
+const formClientes = document.querySelector('#form-clientes');
 
-console.log("ola bom dia viera")
+btnCrearCliente.addEventListener('click', (e) => {
+	e.preventDefault();
+	formClientes.classList.remove('oculto');
+});
